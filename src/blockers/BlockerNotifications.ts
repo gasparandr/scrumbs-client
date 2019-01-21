655f9325c56e87c474a0b13011af3f2a121b8e83
Andrei Gaspar <andrei@planeeet.com>
2019-01-21 20:28:02 +0200

    - Cleanup

--- src/blockers/BlockerNotifications.ts
+++
@@ -1,5 +0,0 @@
-
-
-export class BlockerNotifications {
-    public static readonly EXIT_FINISHED: string = "EXIT_FINISHED";
-}
--- src/prompt/PromptNotifications.ts
+++
@@ -1,5 +0,0 @@
-
-
-export class PromptNotifications {
-    public static DELETE_RECORD: string = "PROMPT_DELETE_RECORD";
-}
