f794270396c30640725f6a5fbc899b156561b6ff
Andrei Gaspar <andrei@planeeet.com>
2019-01-24 21:27:28 +0200

    - Sign up view component members

--- src/authentication/AuthenticationSignUp.ts
+++ src/authentication/AuthenticationSignUp.ts
@@ -1,6 +1,5 @@
 
 
-
 import {View} from "../core/View";
 import {ViewComponent} from "../core/ViewComponent";
 
@@ -19,12 +18,46 @@ const template = require( "../_view-templates/authentication-sign-up.html" );
 
 export class AuthenticationSignUp extends ViewComponent {
 
+    private title: HTMLHeadingElement;
+    private subTitle: HTMLHeadingElement;
+
+    private nameInputLabel: HTMLLabelElement;
+    private nameInput: HTMLInputElement;
+    private nameInputError: HTMLSpanElement;
+
+    private emailInputLabel: HTMLLabelElement;
+    private emailInput: HTMLInputElement;
+    private emailInputError: HTMLSpanElement;
+
+    private passwordInputLabel: HTMLLabelElement;
+    private passwordInput: HTMLInputElement;
+    private passwordInputError: HTMLSpanElement;
+
+
+
 
 
     constructor(view: View, container: HTMLElement) {
         super( view, container );
 
         this.container.innerHTML = template;
+
+        this.title                  = document.getElementById( "" ) as HTMLHeadingElement;
+        this.subTitle               = document.getElementById( "" ) as HTMLHeadingElement;
+
+        this.nameInputLabel         = document.getElementById( "" ) as HTMLLabelElement;
+        this.nameInput              = document.getElementById( "" ) as HTMLInputElement;
+        this.nameInputError         = document.getElementById( "" ) as HTMLSpanElement;
+
+        this.emailInputLabel        = document.getElementById( "" ) as HTMLLabelElement;
+        this.emailInput             = document.getElementById( "" ) as HTMLInputElement;
+        this.emailInputError        = document.getElementById( "" ) as HTMLSpanElement;
+
+        this.passwordInputLabel     = document.getElementById( "" ) as HTMLLabelElement;
+        this.passwordInput          = document.getElementById( "" ) as HTMLInputElement;
+        this.passwordInputError     = document.getElementById( "" ) as HTMLSpanElement;
+
+
 
         this.enterScene();
     }
